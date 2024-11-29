89db6fe7e6a399104ff7c03c3e7184147e905212
31elnomad <103994092+31elnomad@users.noreply.github.com>
2024-11-29 22:47:44 +0100

    Update getdata_api.py

--- scripts/getdata_api.py
+++ scripts/getdata_api.py
@@ -7,14 +7,18 @@ class token:
         self.config = config
         self.access_token = config['data_access']['ACCESS_TOKEN']
         self.api_url = config['data_access']['API_URL']
-        self.start = datetime(int(config['date']['start'][:4]),
-                              int(config['date']['start'][4:6]),
-                              int(config['date']['start'][6:8]),
+        self.start = datetime(int(config['global']['start'][:4]),
+                              int(config['global']['start'][4:6]),
+                              int(config['global']['start'][6:8]),
                               0)
-        self.end = datetime(int(config['date']['end'][:4]),
-                            int(config['date']['end'][4:6]),
-                            int(config['date']['end'][6:8]),
+        self.end = datetime(int(config['global']['end'][:4]),
+                            int(config['global']['end'][4:6]),
+                            int(config['global']['end'][6:8]),
                             0)
+        # Convertir les dates en timestamps UNIX
+        self.start_ts = to_unix_timestamp(self.start.strftime("%Y-%m-%d"))
+        self.end_ts = to_unix_timestamp(self.end.strftime("%Y-%m-%d"))
+        self.scale = config['global']['scale']
 
     def get_mod_device(self):
         url = "https://api.netatmo.com/api/getstationsdata"
@@ -23,7 +27,6 @@ class token:
         data = response.json()
         # Afficher les device_id et module_id
         for station in data['body']['devices']:
-            print(f"Station ID: {station['_id']}")
             self.pres_token = station['_id']
             for module in station['modules']:
                 print(module)
@@ -31,9 +34,37 @@ class token:
                     self.th_token = module['_id']
                 elif 'Rain' in module['data_type']:
                     self.rain_token = module['_id']
-                else:
-                    print(f"  Module ID: {module['_id']}")
+                elif 'Wind' in module['data_type']:
+                    print(module['data_type'])
                     quit()
+                    self.wind_token = module['_id']
+                else:
+                    raise Exception ("Le module {} n'a pas de id".format(module['data_type']))
+
+    def get_historical_data(self, measure_type):
+        if  measure_type = 'th':
+            a = ['Temperature', 'Humidity']
+            module_id = self.th_token
+        elif measure_type = 'rain':
+            a = ['Rain']
+            module_id = self.rain_token
+        headers = {"Authorization": f"Bearer {self.token}"}
+        payload = {
+            "device_id": self.pres_token,
+            "module_id": module_id,
+            "scale": self.scale,
+            "type": a,
+            "date_begin": int(self.start_ts),
+            "date_end": int(self.end_ts),
+            "optimize": "true",
+            "real_time": "false",
+        }
+        response = requests.get(API_URL, headers=headers, params=payload)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Erreur :", response.json())
+            return None
 
     def getdata(self):
         self.get_mod_device()
