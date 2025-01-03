97e66cc61324b76cc443727e6b7433267e57b6e1
31elnomad <103994092+31elnomad@users.noreply.github.com>
2025-01-03 14:42:48 +0100

    Update getdata_api.py

--- scripts/getdata_api.py
+++ scripts/getdata_api.py
@@ -167,7 +167,7 @@ class token:
                 if self.data[name][n[j]] - start_ts < self.scale_sec:
                     time_tmp.append(self.data[name][n[j]])
                     n[j] += 1
-            epoch_timestamp = np.mean(np.array(time_tmp))
+            epoch_timestamp = int(np.mean(np.array(time_tmp)))
             dt_object = datetime.fromtimestamp(epoch_timestamp)  
             formatted_time = dt_object.strftime("%Y-%m-%d %H:%M:%S")
             self.data['Time'][i] = formatted_time
